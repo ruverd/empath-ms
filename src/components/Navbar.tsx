53b821def592147ba64bc46b6c7fe3832440872a
chrislong365 <christopher@chrislong365.com>
2025-02-05 23:06:18 -0800

    adding boilerplage to defeat the blank page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-white shadow p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-lg font-bold">Frontend Challenge</h1>
+      </div>
+    </nav>
+  );
+};
