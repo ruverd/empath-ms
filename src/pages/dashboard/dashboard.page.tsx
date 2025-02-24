62dbf1671ff7cff8706acd974433db9963507314
Ruver Dornelas <ruverd@gmail.com>
2025-02-24 15:26:32 -0300

    Remove Featured Courses section from Dashboard page
    - Commented out SectionFeaturedCourses component
    - Simplified Dashboard page layout by removing unused section

--- src/pages/dashboard/dashboard.page.tsx
+++ src/pages/dashboard/dashboard.page.tsx
@@ -1,7 +1,6 @@
 import { MyProgress } from "@/shared/components/my-progress";
 
 import { SectionAssigned } from "./components/section-assigned";
-import { SectionFeaturedCourses } from "./components/section-featured-courses";
 import { SectionNewCourses } from "./components/section-new-courses";
 import { MOCK_DASHBOARD } from "./dashboard.mock";
 
@@ -11,7 +10,7 @@ export const DashboardPage = () => {
       <div className="flex-1 p-6 flex flex-col gap-4">
         <SectionAssigned />
         <SectionNewCourses />
-        <SectionFeaturedCourses />
+        {/* <SectionFeaturedCourses /> */}
       </div>
       <MyProgress
         progress={MOCK_DASHBOARD.progress}
